eacbc8baca9daf0145d7b7036a588698bff27c8f
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-12-05 14:07:31 +0100

    Rewrite in bash

---
+++ unpack_iso.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Preparing system"
+mkdir -p "/tmp/pop-os/cdrom"
+mkdir "/tmp/pop-os/rootfs"
+sudo modprobe isofs
+
+echo "Downloading pop-os iso"
+curl -L https://iso.pop-os.org/22.04/amd64/intel/17/pop-os_22.04_amd64_intel_17.iso -o /tmp/pop-os/pop-os.iso
+
+echo "Mounting popos iso"
+ISO_MNT=$("losetup -f --show /tmp/pop-os/pop-os.iso")
+mount "$ISO_MNT" /tmp/pop-os/cdrom
+
+echo "Extracting popos squashfs"
+unsquashfs -f -d /tmp/pop-os/rootfs /tmp/pop-os/cdrom/casper/filesystem.squashfs
+
+echo "Updating all packages inside rootfs"
+chroot /tmp/pop-os/rootfs /bin/bash -c "apt update -y && apt upgrade -y"
+
+echo "Compressing rootfs"
+cd "/tmp/pop-os/rootfs"
+tar -cv -I 'xz -9 -T0' -f ../popos-rootfs.tar.xz ./
